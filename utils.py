05b8b8e69e2c739f90cf1cb3e75e8b759cfb38fc
Dylan <61059252+DylanBruner@users.noreply.github.com>
2023-03-31 15:00:50 -0400

    Some background art

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-import pygame
+import pygame, numpy, tqdm
+import numba, os
+from numba import jit
+from PIL import Image
 
 class Util:
     @staticmethod
@@ -10,3 +13,31 @@ class Util:
         for i in range(spriteCount):
             sprites.append(spritesheet.subsurface((i * spriteSize[0], 0, spriteSize[0], spriteSize[1])))
         return sprites
+
+    @jit(nopython=True)
+    def generateDiffuseMap(size: tuple, items: list[int]) -> list[list]:
+        r_map = numpy.zeros((size[0], size[1]), dtype=numpy.int8)
+        for x in range(size[0]):
+            for y in range(size[1]):
+                r_map[x][y] = items[numpy.random.randint(0, len(items))]
+                
+        return r_map
+
+    # jit with object mode by default
+    @jit(nopython=False, cache=True, parallel=True)
+    def generateMeshedImage(size: tuple, diffuseMap: list[list], textures: list[Image.Image]) -> Image.Image:
+        # check if cached
+        if os.path.exists(f"data/cache/{size[0]}x{size[1]}_titlemesg.png"):
+            return Image.open(f"data/cache/{size[0]}x{size[1]}_titlemesg.png")
+        meshedImage = Image.new("RGBA", size)
+        progbar = tqdm.tqdm(total=size[0] * size[1], desc="Generating meshed image")
+        for x in range(size[0]):
+            for y in range(size[1]):
+                meshedImage.paste(textures[diffuseMap[x][y]], (x, y))
+                progbar.update(1)
+        
+        f = open(f"data/cache/{size[0]}x{size[1]}_titlemesg.png", "wb")
+        meshedImage.save(f, "PNG")
+        f.close()
+        
+        return meshedImage
--- viewports/mainmenu.py
+++ viewports/mainmenu.py
@@ -1,6 +1,8 @@
-import pygame
+import pygame, time
 from componentsystem import Viewport
 from components import *
+from utils import Util
+from PIL import Image
 
 class MainMenu(Viewport):
     def __init__(self, size: tuple[int, int]):
@@ -10,6 +12,23 @@ class MainMenu(Viewport):
         self.theme = menutheme
 
         self.setupMenu()
+        self.generateBackground()
+
+    def generateBackground(self):
+        startTime = time.time()
+        self.DIFFUSE_MAP = Util.generateDiffuseMap(self.size, [0, 1])
+        print("Generated diffuse map in", time.time() - startTime, "seconds")
+        # self.GRASS_TEXTURE_0 = pygame.image.load("data/assets/world/grass/1.jpg")
+        # self.GRASS_TEXTURE_1 = pygame.image.load("data/assets/world/grass/2.jpg")
+        self.GRASS_TEXTURE_0 = Image.open("data/assets/world/grass/1.jpg")
+        self.GRASS_TEXTURE_1 = Image.open("data/assets/world/grass/2.jpg")
+
+        startTime = time.time()
+        meshed = Util.generateMeshedImage(self.size, self.DIFFUSE_MAP, [self.GRASS_TEXTURE_0, self.GRASS_TEXTURE_1])
+        print("Generated meshed image in", time.time() - startTime, "seconds")
+        self.MAP_SURFACE = pygame.Surface(self.size)
+        self.MAP_SURFACE.blit(pygame.image.fromstring(meshed.tobytes(), meshed.size, meshed.mode), (0, 0))
+        # do this more efficiently
 
     def setupMenu(self):
         y_offset = self.size[1] / 4 - 100
@@ -18,16 +37,20 @@ class MainMenu(Viewport):
         self.options_button = Button((self.size[0] / 2 - 100, 200 + y_offset), (200, 40), "Options")
         self.quit_button = Button((self.size[0] / 2 - 100, 250 + y_offset), (200, 40), "Quit")
 
+        self.quit_button.on_click = lambda: quit()
+
         self.registerComponents([self.menu_text, self.play_button, self.options_button, self.quit_button])
     
     def resize(self, old: tuple[int, int], new: tuple[int, int]):
         self.size = new
-        # unregister all components
         self.components['components'] = []
         self.components['hooks'] = {}
         self.setupMenu()
+        self.generateBackground()
 
     def draw(self, enviorment: dict):
+        # draw background
+        enviorment["window"].blit(self.MAP_SURFACE, (0, 0))
         super().draw(enviorment)
         
         # custom draw code go here
