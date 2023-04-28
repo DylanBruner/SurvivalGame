2eeee7cd40df02e16c89dc82a7710a4d2f7242d3
Dylan <61059252+DylanBruner@users.noreply.github.com>
2023-04-28 12:27:32 -0400

    Crafting and stuff

--- game/playerstorage.py
+++ game/playerstorage.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame, json
 from game.keybinding import Bindings
 from game.world import TileIDS, TEXTURE_MAPPINGS
+from utils import Util
 import game.particlesystem as pSys
 
 class Config:
@@ -34,7 +35,8 @@ class PlayerStorage:
         self.slotLocations = [[None for _ in range(14)] for _ in range(4)]
 
         self.craftingSlots = [[None for _ in range(3)] for _ in range(3)]
-        self.outputSlot    = (1, 1)
+        self.outputSlot    = (0, 0)
+        self.currentRecipe = None
 
         self.dragging: bool              = False
         self.dragOrigin: tuple[int, int] = None
@@ -42,6 +44,7 @@ class PlayerStorage:
 
         self.QUANTITY_FONT = pygame.font.SysFont("Arial", 16)
 
+    @Util.MonkeyUtils.autoErrorHandling
     def draw(self, surf: pygame.Surface, env: dict) -> None:
         # 400 tall and 450 wide white rect that's centered
         width, height = 600, 400
@@ -70,25 +73,73 @@ class PlayerStorage:
                 surf.blit(self.slot, (topLeft[0] + 20 + (x * 40), topLeft[1] + 20 + (y * 40)))
                 if self.craftingSlots[y][x] is not None:
                     surf.blit(pygame.transform.scale(TEXTURE_MAPPINGS[self.craftingSlots[y][x][0]], (32, 32)), (topLeft[0] + 20 + (x * 40) + 4, topLeft[1] + 20 + (y * 40) + 4)) 
+                    # draw the quantity in the top left
+                    if self.craftingSlots[y][x][1] > 1:
+                        text = self.QUANTITY_FONT.render(str(self.craftingSlots[y][x][1]), True, (255, 255, 255))
+                        surf.blit(text, (topLeft[0] + 26 + (x * 40), topLeft[1] + 26 + (y * 40)))
 
         # draw the output slot
         slotLocation = (270, 120)
         surf.blit(self.slot, slotLocation)
 
-        if self.outputSlot is not None:
+        if self.outputSlot is not None and self.outputSlot != (0, 0):
             surf.blit(pygame.transform.scale(TEXTURE_MAPPINGS[self.outputSlot[0]], (32, 32)), (slotLocation[0] + 4, slotLocation[1] + 4)) 
 
         
         if self.dragging:
             surf.blit(TEXTURE_MAPPINGS[self.dragItem[0]], pygame.mouse.get_pos())
+            # draw the quantity in the top left
+            if self.dragItem[1] > 1:
+                text = self.QUANTITY_FONT.render(str(self.dragItem[1]), True, (255, 255, 255))
+                surf.blit(text, pygame.mouse.get_pos())
 
+    @Util.MonkeyUtils.autoErrorHandling
     def getSlotLocation(self, pos: tuple[int, int]) -> tuple[int, int]:
         for y in range(len(self.slotLocations)):
             for x in range(len(self.slotLocations[y])):
                 if self.slotLocations[y][x][0] < pos[0] < self.slotLocations[y][x][0] + self.slot.get_width() and self.slotLocations[y][x][1] < pos[1] < self.slotLocations[y][x][1] + self.slot.get_height():
                     return x, y
         return (-1, -1)
+    
+    @Util.MonkeyUtils.autoErrorHandling
+    def updateCrafting(self) -> None:
+        with open('data/config/recipes.json', 'r') as f:
+            recipes = json.load(f)
 
+        for recipe in recipes:
+            if not recipe["shapeless"]:
+                for y in range(len(self.craftingSlots)):
+                    for x in range(len(self.craftingSlots[y])):
+                        if recipe["recipe"][y][x] != [0, 0] and self.craftingSlots[y][x] is not None:
+                            if recipe["recipe"][y][x][0] > self.craftingSlots[y][x][0] or recipe["recipe"][y][x][1] > self.craftingSlots[y][x][1]: 
+                                break
+                        elif recipe["recipe"][y][x] != [0, 0] and self.craftingSlots[y][x] is None:
+                            break
+                        elif recipe["recipe"][y][x] == [0, 0] and self.craftingSlots[y][x] is not None:
+                            break
+                    else:
+                        continue
+                    break
+                else:
+                    self.outputSlot = (recipe["result"]["id"], recipe["result"]["count"])
+                    self.currentRecipe = recipe
+                    return
+        
+        self.outputSlot = None
+    
+    @Util.MonkeyUtils.autoErrorHandling
+    def craft(self) -> None:
+        # remove the amount of items from the crafting slots
+        for y in range(len(self.craftingSlots)):
+            for x in range(len(self.craftingSlots[y])):
+                if self.craftingSlots[y][x] is not None:
+                    self.craftingSlots[y][x] = (self.craftingSlots[y][x][0], self.craftingSlots[y][x][1] - self.currentRecipe["recipe"][y][x][1])
+                    if self.craftingSlots[y][x][1] == 0:
+                        self.craftingSlots[y][x] = None
+        self.updateCrafting()
+
+
+    @Util.MonkeyUtils.autoErrorHandling
     def addItem(self, item: Item):
         for y in range(len(self.inventory)):
             for x in range(len(self.inventory[y])):
@@ -98,7 +149,9 @@ class PlayerStorage:
                 
                 elif self.inventory[y][x][0] == item.item_id and self.inventory[y][x][1] + item.count <= 250:
                     self.inventory[y][x] = (item.item_id, self.inventory[y][x][1] + item.count)
+                    return
 
+    @Util.MonkeyUtils.autoErrorHandling
     def onEvent(self, event: pygame.event.Event) -> None:
         # if it's mousedown and the mouse is in the inventory get the selected slot
         if event.type == pygame.MOUSEBUTTONDOWN and self.open:
@@ -106,17 +159,81 @@ class PlayerStorage:
             slotX, slotY = self.getSlotLocation(event.pos)
             if slotX == -1 and slotY == -1:
                 # check if it's one of the crafting slots
-                found = False
                 for y in range(len(self.craftingSlots)):
                     for x in range(len(self.craftingSlots[y])):
                         topLeft = (100, 60)
                         loc     = (topLeft[0] + 20 + (x * 40), topLeft[1] + 20 + (y * 40))
                         mouse   = pygame.mouse.get_pos()
 
-                        #TODO: Fix this mouse click detection code
-                        if (mouse[0] >= loc[0] and mouse[0] <= loc[1] + 32) and (mouse[1] >= loc[0] and mouse[1] <= loc[1] + 32):
-                            print(x, y)
+                        transferMode = -1
+                        if event.button == 1: # left click, whole stack
+                            transferMode = 0
+                        elif event.button == 3: # right click, 1 item
+                            transferMode = 1
+                        
+                        if transferMode == -1: continue
 
+                        # this code sucks but whatever
+                        if loc[0] < mouse[0] < loc[0] + self.slot.get_width() and loc[1] < mouse[1] < loc[1] + self.slot.get_height():
+                            # if we are holding an item and the slot is empty then put the item in the slot (1 item if right click, whole stack if left click)
+                            if self.dragging and self.craftingSlots[y][x] is None:
+                                if transferMode == 0:
+                                    self.craftingSlots[y][x] = self.dragItem
+                                    self.dragging = False
+                                    self.updateCrafting()
+                                elif transferMode == 1:
+                                    self.craftingSlots[y][x] = (self.dragItem[0], 1)
+                                    self.dragItem = (self.dragItem[0], self.dragItem[1] - 1)
+                                    if self.dragItem[1] <= 0:
+                                        self.dragging = False
+                                    self.updateCrafting()
+                                    break
+                            
+                            # if the slot is not empty and we are holding an item then try to stack the items, (1 item if right click, whole stack if left click)
+                            elif self.dragging and self.craftingSlots[y][x] is not None:
+                                if self.craftingSlots[y][x][0] == self.dragItem[0] and self.craftingSlots[y][x][1] < 250:
+                                    if transferMode == 0:
+                                        self.craftingSlots[y][x] = (self.dragItem[0], self.craftingSlots[y][x][1] + self.dragItem[1])
+                                        self.dragging = False
+                                        self.updateCrafting()
+                                    elif transferMode == 1:
+                                        self.craftingSlots[y][x] = (self.dragItem[0], self.craftingSlots[y][x][1] + 1)
+                                        self.dragItem = (self.dragItem[0], self.dragItem[1] - 1)
+                                        if self.dragItem[1] <= 0:
+                                            self.dragging = False
+                                        self.updateCrafting()
+                                        break
+                            
+                            # if the slot is not empty and we are not holding an item then pick up the item (1 item if right click, whole stack if left click)
+                            elif not self.dragging and self.craftingSlots[y][x] is not None:
+                                if transferMode == 0:
+                                    self.dragItem = self.craftingSlots[y][x]
+                                    self.dragging = True
+                                    self.updateCrafting()
+
+                                    self.dragging = True
+                                    self.dragItem = self.craftingSlots[y][x]
+                                    self.craftingSlots[y][x] = None
+                                elif transferMode == 1:
+                                    self.dragItem = (self.craftingSlots[y][x][0], 1)
+                                    self.craftingSlots[y][x] = (self.craftingSlots[y][x][0], self.craftingSlots[y][x][1] - 1)
+                                    self.dragging = True
+                                    self.dragItem = (self.craftingSlots[y][x][0], 1)
+                                    
+                                    if self.craftingSlots[y][x][1] <= 0:
+                                        self.craftingSlots[y][x] = None
+                                    self.updateCrafting()
+                                    break
+
+                            self.updateCrafting()
+                            break
+            
+                # check if the output slot was clicked, 270, 100
+                if 270 < event.pos[0] < 270 + self.slot.get_width() and 100 < event.pos[1] < 100 + self.slot.get_height() + 20:
+                    if self.outputSlot is not None:
+                        item = Item(self.outputSlot[0], self.outputSlot[1], TEXTURE_MAPPINGS[self.outputSlot[0]])
+                        self.addItem(item)
+                        self.craft()
                 return
 
             # if the slot is empty and the player is holding an item
@@ -152,7 +269,6 @@ class PlayerStorage:
                 self.dragItem = self.inventory[slotY][slotX]
                 self.inventory[slotY][slotX] = (0, 0)
             
-            # if key 1-9 is pressed and the player is hovering over a slot print hi
             keys = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9]
             if self.getSlotLocation(event.pos)[1] != -1 and any(pygame.key.get_pressed()[key] for key in keys):
                 keyPressed = [key for key in keys if pygame.key.get_pressed()[key]][0]
